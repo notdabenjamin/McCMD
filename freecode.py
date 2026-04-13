47794a191f2daa48a7f9c1e275919b70b51b2e0a
NotDaBenjamin <benjaminlevikrahn@gmail.com>
2026-04-13 07:14:30 -0500

    Implement preview message and return to menu
    Add preview message and menu navigation in freecode.py

---
+++ freecode.py
@@ -0,0 +1,4 @@
+print("\n" * 100)
+print("You are currently viewing a preview of McCMD, this is not the full application,\nask questions by doing //contact in the menu. Thank you for being interested in the Python Command Block.\nFor more information, go to https://pythoncommandblock.pages.dev/ or the Official GitHub Page.")
+input("\n\nPress enter to return to the menu")
+exec(open("menu.py").read())
